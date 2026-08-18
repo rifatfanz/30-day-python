8768e88fbad21a0318d7a15fe6c0ded40a569454
Mahedi Hasan Rifat <mdmhrifat007@gmail.com>
2026-08-18 22:59:32 +0600

    Day-1-Variable-input-print

---
+++ Day-1-Variable-input-print.py
@@ -0,0 +1,6 @@
+Nam = input("what's your name? ")
+job = "Student "
+Department = input("Which Department Are you in? ")
+Dob = input("Write your Date of Birth Correctly YYYY-MM-DD: ")
+
+print(f"Your name is {Nam}. You are a {job} in Department of {Department}. Your Date Of Birth is {Dob}")
